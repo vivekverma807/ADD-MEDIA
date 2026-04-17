164eaba7797215c6b099f2ec65f50c2d9c7b180f
Vivek Kumar Verma <vivekverma502807@gmail.com>
2026-04-18 01:23:18 +0530

    feat: implement mobile navigation, PWA install prompt, and responsive hero component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import Showcase from './pages/Showcase';
 import Order from './pages/Order';
 import AdminLogin from './pages/Admin/Login';
 import AdminDashboard from './pages/Admin/Dashboard';
+import { MobileNav } from './components/MobileNav';
+import { InstallPrompt } from './components/InstallPrompt';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/showcase" element={<Showcase />} />
-        <Route path="/order" element={<Order />} />
-        
-        {/* Admin Routes */}
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        
-        {/* Fallback */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen pb-16 lg:pb-0">
+        <InstallPrompt />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/showcase" element={<Showcase />} />
+          <Route path="/order" element={<Order />} />
+          
+          {/* Admin Routes */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          
+          {/* Fallback */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+        <MobileNav />
+      </div>
     </Router>
   );
 }
