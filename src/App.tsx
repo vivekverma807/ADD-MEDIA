cdce787a193a4de31caf1f7a323454d11044acd2
Vivek Kumar Verma <vivekverma502807@gmail.com>
2026-05-02 12:59:16 +0530

    Add Track page route
    Import the Track component and add a new <Route path="/track"> in src/App.tsx so the app can navigate to the tracking page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Showcase from './pages/Showcase';
 import Order from './pages/Order';
+import Track from './pages/Track';
 import AdminLogin from './pages/Admin/Login';
 import AdminDashboard from './pages/Admin/Dashboard';
 import Privacy from './pages/Legal/Privacy';
@@ -14,12 +15,13 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div className="flex flex-col min-h-screen pb-16 lg:pb-0">
+      <div className="flex flex-col min-h-screen pb-24 lg:pb-0">
         <InstallPrompt />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/showcase" element={<Showcase />} />
           <Route path="/order" element={<Order />} />
+          <Route path="/track" element={<Track />} />
           
           {/* Admin Routes */}
           <Route path="/admin/login" element={<AdminLogin />} />
